7aeb286840471e947612b8c760f95f5759b85a7f
Agustiawan <44895587+thype5209@users.noreply.github.com>
2023-07-03 08:26:50 +0800

    clear image

--- src/components/Categories.vue
+++ src/components/Categories.vue
@@ -1,9 +1,9 @@
 <template>
-    <div class="container py-16" v-if="categoris.length > 0">
+    <div class="container py-6" v-if="categoris.length > 0">
         <h2 class="text-2xl font-medium text-gray-800 uppercase mb-6">shop by category</h2>
-        <div class="grid grid-cols-3 gap-3">
-            <div class="relative rounded-sm overflow-hidden group" v-for="item in categoris" :key="item.id">
-                <img :src="item.thumbnail" alt="category 1" class="w-full">
+        <div class="grid  gap-3 h-max" :class="categoris.length >= 5 ? 'grid-cols-3 md:grid-cols-5' :'grid-cols-3 md:grid-cols-7'">
+            <div class="relative rounded-sm overflow-hidden group flex items-center justify-center" v-for="item in categoris" :key="item.id">
+                <img :src="item.thumbnail" alt="category 1" class="w-full object-cover relative">
                 <router-link :to="{name: 'shop', params: {slug: item.name } }"
                     class="absolute inset-0 bg-black bg-opacity-40 flex items-center justify-center text-xl text-white font-roboto font-medium group-hover:bg-opacity-60 transition">{{ item.name }}</router-link>
             </div>
